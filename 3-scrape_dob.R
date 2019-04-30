26cdfe52c5e4665d2226ac35317ff6b9fc48265d
Sara Khan <skhan81.900@gmail.com>
2019-04-30 00:47:27 -0400

    Add files via upload

--- 3-scrape_dob.R
+++ 3-scrape_dob.R
@@ -9,13 +9,12 @@ nba<-readxl::read_excel("data/nba.xlsx")
 
 players_info<-list()
 
-for(i in 1:nrow(nba))
+for(i in 1:6)
 {
-  float<-nba[i,2]$URL
+  float<-del.me[i,2]$URL
  # float <- paste("squad", i, sep ="")
  # print(float)
-  html = read_html(paste0("https://en.wikipedia.org","/wiki/Jazmon_Gwathmey"))
-  
+  html = read_html(paste0("https://en.wikipedia.org",float))
   
   # tt <- try(html_nodes(html, "table")[[1]])
   # ifelse(is(tt,"try-error"),"There was a warning or an error","OK")
@@ -50,17 +49,41 @@ for(i in 1:nrow(nba))
 
 #players_info2<-do.call(rbind.data.frame, players_info) %>% rownames_to_column()
 
-# del.me<-nba %>% anti_join(players_info_all, by=c("URL"="Player")) %>% 
-#   # dropping one record, as it was having a faulty wiki link
-#   filter(!Link == "Jazmon Gwathmey")
+
+players_info6<-do.call(rbind.data.frame, players_info) %>% rownames_to_column()
+
+players_info_all5<-players_info_all4 %>% 
+  # for some reason, "Bealton, VA" came in all the rows. Removing this
+  filter(!str_detect(PlayerInfo, "Bealeton")) %>% 
+  # only get the player information with "DOB"/Born information
+  filter(Info=="Born") %>% 
+  rbind(players_info6)
+
+
+del.me<-nba %>% anti_join(players_info6 %>%   filter(Info=="Born") %>% 
+                            filter(!str_detect(PlayerInfo, "Bealeton")) , by=c("URL"="Player")) %>%
+  # dropping one record, as it was having a faulty wiki link
+  filter(!Link == "Jazmon Gwathmey")
+
+del.me<-nba %>%
+  filter(!Link=="Main page") %>% 
+  
+  anti_join(players_info_all5 %>%   filter(Info=="Born") %>% 
+                            filter(!str_detect(PlayerInfo, "Bealeton")) , by=c("URL"="Player")) %>%
+  # dropping one record, as it was having a faulty wiki link
+  filter(!Link == "Jazmon Gwathmey")
+
+
+
 
 players_info_all<-rbind(players_info2,
                         players_info3,
                         players_info4,
                         players_info5) %>% unique()
 
-saveRDS(players_info_all,"data/players_info_all.RDS")
 
+saveRDS(players_info_all5,"data/players_info_all.RDS")
+players_info_all<-players_info_all5
 
 library(stringi)
 detach("package:dplyr", unload=TRUE)
@@ -84,6 +107,13 @@ wnba_dob<-players_info_all %>%
   select(DOB_year, Player=PlayerName3) %>% unique() %>% 
   group_by(Player) %>% 
   # finally, join to the wnba.pers dataset
-  right_join(wnba.pers)
+  right_join(wnba.pers) %>% 
+  mutate(Age = ifelse(is.na(DOB_year),NA,
+                      Year - as.numeric(DOB_year)))
 
 
+table((wnba_dob %>% select(Player, DOB_year) %>% unique() %>% mutate(flag = ifelse(is.na(DOB_year),
+                                                                                   "Missing DOB",
+                                                                                    "Not missing DOB")))$flag)
+# majority are missing
+# now majority are not missing!
